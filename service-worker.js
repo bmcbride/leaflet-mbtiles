59134793e2caff7f8a79c56e24b6b60efef73bed
Bryan McBride <mcbride.bryan@gmail.com>
2019-01-04 15:54:05 -0500

    bump cache version

--- service-worker.js
+++ service-worker.js
@@ -10,7 +10,7 @@
  See the License for the specific language governing permissions and
  limitations under the License.
 */
-const PRECACHE = 'precache-01.04.19';
+const PRECACHE = 'precache-01.04.19.2';
 const RUNTIME = 'runtime';
 
 // A list of local resources we always want to be cached.
