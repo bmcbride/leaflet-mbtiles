4e98724c00ee0d059c2b3b9f8716df607bdf6c90
Bryan McBride <mcbride.bryan@gmail.com>
2019-12-13 17:58:29 -0500

    add aerial imagery layer

--- service-worker.js
+++ service-worker.js
@@ -10,7 +10,7 @@
  See the License for the specific language governing permissions and
  limitations under the License.
 */
-const PRECACHE = 'precache-12.06.19.2';
+const PRECACHE = 'precache-12.13.19.1';
 const RUNTIME = 'runtime';
 
 // A list of local resources we always want to be cached.
