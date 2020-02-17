a315f41d7035dfa4d5f63e7b2be97a87343fa6c3
Bryan McBride <mcbride.bryan@gmail.com>
2020-02-17 18:58:48 -0500

    Lighthouse PWA optimizations
    Add 512 png
    Add theme-color meta tag
    Add <noscript> tag

--- service-worker.js
+++ service-worker.js
@@ -10,7 +10,7 @@
  See the License for the specific language governing permissions and
  limitations under the License.
 */
-const PRECACHE = 'precache-02.17.20.7';
+const PRECACHE = 'precache-02.17.20.8';
 const RUNTIME = 'runtime';
 
 // A list of local resources we always want to be cached.
@@ -21,6 +21,7 @@ const PRECACHE_URLS = [
   'assets/img/apple-touch-icon.png',
   'assets/img/favicon-32x32.png',
   'assets/img/favicon-16x16.png',
+  'assets/img/favicon-512x512.png',
   'assets/img/android-chrome-192x192.png',
   'assets/vendor/leaflet-1.6.0/images/layers.png',
   'assets/vendor/leaflet-1.6.0/images/layers-2x.png',
